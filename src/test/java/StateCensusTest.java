841ca68ea8ae220b590e03e55894becbe5ada58f
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-04-07 22:40:10 +0530

    Added:Test Case For Improper File Extension

--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -7,6 +7,7 @@ public class StateCensusTest
 {
     public static String CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
     public static String CSV_FILE_PATH_FOR_WRONG_FILE = "./src/test/resources/StateCensus.csv";
+    public static String CSV_FILE_PATH_FOR_WRONG_FILE_EXTENSION = "./src/test/resources/StateCensus.jpg";
     //test will pass when totalNumberOfRecords are 29
     @Test
     public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws IOException, CustomExceptions
@@ -16,13 +17,12 @@ public class StateCensusTest
         Assert.assertEquals(29, totalNumberOfRecords);
     }
 
-    //Test will pass only after giving proper name to file
+    //Test for improper file name
     @Test
     public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() throws IOException,CustomExceptions
     {
         try
         {
-           // StateCensusAnalyser censusAnalyserObject = new StateCensusAnalyser(CSV_FILE_PATH);
             StateCensusAnalyser censusAnalyserObject = new StateCensusAnalyser(CSV_FILE_PATH_FOR_WRONG_FILE);
             censusAnalyserObject.loadData();
         }
@@ -30,6 +30,22 @@ public class StateCensusTest
         {
             Assert.assertEquals(CustomExceptions.TypeOfException.NO_FILE_FOUND,e.typeOfException);
         }
-
     }
+
+    //Test Case 1.3 Name Correct but type Incorrect
+    @Test
+    public void givenStateCensusAnalyserFile_WhenImproperFileExtension_ReturnsException() throws IOException,CustomExceptions
+    {
+        try
+        {
+            StateCensusAnalyser censusAnalyserObject = new StateCensusAnalyser(CSV_FILE_PATH_FOR_WRONG_FILE_EXTENSION);
+            censusAnalyserObject.loadData();
+        }
+        catch (CustomExceptions e)
+        {
+            Assert.assertEquals(CustomExceptions.TypeOfException.NO_FILE_FOUND,e.typeOfException);
+        }
+    }
+
+
 }

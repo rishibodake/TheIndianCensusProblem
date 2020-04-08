fcb1a126b56d53a1a07ff5029c41a721633383fd
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-04-08 21:48:09 +0530

    Refactor 3 Introduced the Interface and then Implemented

--- src/main/java/CSVInterface.java
+++ src/main/java/CSVInterface.java
@@ -1,6 +1,6 @@
 import java.io.Reader;
 import java.util.Iterator;
-
+//Should have Introduced in Refactor 3 but insted in 2
 public class CSVInterface {
     public <E> Iterator<E> getCSVfileIterator(Reader reader, Class<E> csvClass) throws CustomExceptions {
         return null;
